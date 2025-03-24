2561c92c4fdc85a28a52b2a8634d36ebfdcf62b9
MatteoOnger <matteo.onger@studenti.unimi.it>
2025-03-24 23:36:10 +0100

    refac(AvoidingArena): renaming

--- pacman/arena.py
+++ pacman/arena.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import os
 
+from enum import Enum
 from gym import spaces
 from typing import *
 
@@ -22,31 +23,36 @@ class AvoidingArena(gym.Env):
     metadata = {"render_modes": ["ansi", "human", "rgb_array"], "render_fps": 4}
 
 
+    class Actions(Enum):
+        RIGHT = 0
+        UP = 1
+        LEFT = 2
+        DOWN = 3
+
+
     def __init__(
         self,
-        render_mode :str|None=None,
-        grid_dim :Tuple[int,int]=(5, 5),
         cell_size :int=64,
+        grid_dim :Tuple[int,int]=(5, 5),
         max_num_moves :int=10,
-        num_enemies :int=5,
+        num_enemies :int=3,
         default_reward :float=0.0,
         on_success_reward :float=1.0,
         on_failure_reward :float=-1.0,
         remain_unchanged_reward :float=0.0,
+        render_mode :str|None=None,
     ):
         """
         Parameters
         ----------
-        render_mode : str | None, optional
-            Render mode to help visualise what the agent sees, by default ``None``.
-        grid_dim : Tuple[int, int], optional
-            A tuple of two integers for ``(grid_x, grid_y)``, by default ``(5, 5)``.
         cell_size : float, optional
             The side length of each cell in pixels, by default ``64``.
+        grid_dim : Tuple[int, int], optional
+            A tuple of two integers for ``(grid_x, grid_y)``, by default ``(5, 5)``.
         max_num_moves : int, optional
             Maximum number of moves to solve the game, by default ``10``.
         num_enemies : int, optional
-            Number of enemies, by default ``5``.
+            Number of enemies, by default ``3``.
         default_reward : float, optional
             Default reward, by default ``0.0``.
         on_success_reward : float, optional
@@ -55,6 +61,8 @@ class AvoidingArena(gym.Env):
             Reward if hit by an enemy, by default ``-1.0``.
         remain_unchanged_reward : float, optional
             Reward for staying in the same position, by default ``0.0``.
+        render_mode : str | None, optional
+            Render mode to help visualise what the agent sees, by default ``None``.
 
         Raises
         ------
@@ -65,18 +73,18 @@ class AvoidingArena(gym.Env):
         """
         if num_enemies > (grid_dim[0] * grid_dim[1] - 2):
             raise ValueError("Too many enemies")
-        if render_mode is not None and render_mode not in self.metadata["render_modes"]:
-            raise ValueError("Render mode must be None, 'human' or 'rgb_array'")
+        if (render_mode is not None) and (render_mode not in self.metadata["render_modes"]):
+            raise ValueError("Render mode must be `None` or in `metadata['render_modes']`")
 
-        self.render_mode = render_mode
-        self.grid_dim = np.array(grid_dim)
         self.cell_size = cell_size
+        self.grid_dim = np.array(grid_dim)
         self.max_num_moves = max_num_moves
         self.num_enemies = num_enemies    
         self.default_reward = default_reward
         self.on_success_reward = on_success_reward
         self.on_failure_reward = on_failure_reward
         self.remain_unchanged_reward = remain_unchanged_reward
+        self.render_mode = render_mode
 
         self.grid_x, self.grid_y = grid_dim
         self.window_size_w, self.window_size_h = self.grid_x * self.cell_size, self.grid_y * self.cell_size
@@ -85,8 +93,8 @@ class AvoidingArena(gym.Env):
         self.observation_space = spaces.Dict(
             {
                 "agent": spaces.Box(0, self.grid_dim - 1, dtype=int),
-                "goal": spaces.Box(0, self.grid_dim -1 , dtype=int),
-                "enemies": spaces.Tuple(spaces.Box(0, 5 , dtype=int) for _ in range(3))
+                "target": spaces.Box(0, self.grid_dim - 1, dtype=int),
+                "enemies": spaces.Tuple(spaces.Box(0, self.grid_dim - 1, dtype=int) for _ in range(self.num_enemies))
             }
         )
 
@@ -98,45 +106,39 @@ class AvoidingArena(gym.Env):
             2: np.array([-1, 0]),
             3: np.array([0, -1]),
         }
-        self.str_to_action = {
-            "right": 0,
-            "up": 1,
-            "left": 2,
-            "down": 3
-        }
 
-        # initialize environment states
-        self.start_pos = None
-        self.curr_pos = None
-        self.goal_pos = None
-        self.enemies = None
-        self.moves_counter = None
+        # initialize environment state
+        self._start_pos = None
+        self._curr_pos = None
+        self._target_pos = None
+        self._enemies_pos = None
+        self._moves_counter = None
 
-        #  background agent, goal and enemy images
-        self.background_image = None
-        self.goal_image = None
-        self.agent_image = None
-        self.enemy_images = None 
+        #  background agent, target and enemy images
+        self._background_image = None
+        self._agent_image = None
+        self._target_image = None
+        self._enemy_images = None 
 
         # if human-rendering is used, <self.window> will be a reference
         # to the window that we draw to. <self.clock> will be a clock that is used 
         # to ensure that the environment is rendered at the correct framerate 
-        self.window_surface = None
-        self.clock = None
+        self._window_surface = None
+        self._clock = None
         return
 
 
     def close(self) -> None:
         """
-        After the user has finished using the environment, close contains the code necessary to "clean up" the environment.
+        After the user has finished using the environment, close contains the code necessary to clean up the environment.
         This is critical for closing rendering windows, database or HTTP connections.
         Calling ``close`` on an already closed environment has no effect and won't raise an error.
         """
-        if self.window_surface is not None and self.render_mode in {"human", "rgb_array"}:
+        if self._window_surface is not None and self.render_mode in {"human", "rgb_array"}:
             try:
                 import pygame
             except ImportError:
-                raise ImportError("pygame is not installed") from None
+                raise ImportError("Pygame is not installed") from None
 
             pygame.display.quit()
             pygame.quit()
@@ -188,24 +190,24 @@ class AvoidingArena(gym.Env):
         empty_pos = [np.array([i,j]) for i in range(self.grid_x) for j in range(self.grid_y)]
 
         # init counter
-        self.moves_counter = 0
+        self._moves_counter = 0
 
         # generate start position
         idx = self.np_random.integers(0, len(empty_pos))
-        self.start_pos = empty_pos[idx]
+        self._start_pos = empty_pos[idx]
         del empty_pos[idx]
 
         # set current position
-        self.curr_pos = self.start_pos
+        self._curr_pos = self._start_pos
 
         # generate end position
         idx = self.np_random.integers(0, len(empty_pos))
-        self.goal_pos = empty_pos[idx]
+        self._target_pos = empty_pos[idx]
         del empty_pos[idx]
 
         # generate enemy positions
         idxs = self.np_random.choice(len(empty_pos), size=self.num_enemies, replace=False)
-        self.enemies = np.array([empty_pos[idx] for idx in idxs])
+        self._enemies_pos = np.array([empty_pos[idx] for idx in idxs])
 
         if self.render_mode == "human":
             self._render_frame()
@@ -234,29 +236,28 @@ class AvoidingArena(gym.Env):
             If true, the user needs to call the method ``reset``.
         truncated : bool
             Whether the truncation condition outside the scope of the MDP is satisfied, typically, this is a timelimit.
-            Currently always set to ``False``.
         info : Dict[str,Any]
             Contains auxiliary diagnostic information (helpful for debugging, learning, and logging).
         """
-        prev_pos = self.curr_pos
+        prev_pos = self._curr_pos
         direction = self.action_to_direction[action]
 
         # update agent position
-        self.curr_pos = np.clip(
-        self.curr_pos + direction, 0, self.grid_dim - 1
+        self._curr_pos = np.clip(
+        self._curr_pos + direction, 0, self.grid_dim - 1
         )
 
         # update moves counter
-        self.moves_counter += 1
-        truncated = self.moves_counter >= self.max_num_moves
+        self._moves_counter += 1
+        truncated = self._moves_counter >= self.max_num_moves
 
         # compute the reward
         reward, terminated = self.default_reward, False
-        if (self.curr_pos == self.enemies).all(axis=1).any():
+        if (self._curr_pos == self._enemies_pos).all(axis=1).any():
             reward, terminated = self.on_failure_reward, True
-        elif (self.curr_pos == self.goal_pos).all():
+        elif (self._curr_pos == self._target_pos).all():
             reward, terminated = self.on_success_reward, True
-        elif (self.curr_pos == prev_pos).all():
+        elif (self._curr_pos == prev_pos).all():
             reward, terminated = self.remain_unchanged_reward, False
 
         if self.render_mode == "human":
@@ -273,7 +274,7 @@ class AvoidingArena(gym.Env):
         :Dict[str, Any]
             Auxiliary infomration.
         """
-        return {"manhattan_distance": np.linalg.norm(self.curr_pos - self.goal_pos, ord=1)}
+        return {"manhattan_distance": np.linalg.norm(self.curr_pos - self.goal_pos, ord=1), "moves_counter": self._moves_counter}
 
 
     def _get_obs(self) -> Dict[str,Any]:
@@ -285,7 +286,7 @@ class AvoidingArena(gym.Env):
         :Dict[str, Any]
             Observation.
         """
-        return {"agent": self.curr_pos, "goal": self.goal_pos, "enemies": self.enemies}
+        return {"start": self._start_pos, "agent": self._curr_pos, "target": self._target_pos, "enemies": self._enemies_pos}
 
 
     def _render_frame(self) -> np.ndarray|None:
@@ -308,30 +309,25 @@ class AvoidingArena(gym.Env):
             raise ImportError("Pygame is not installed") from None
 
         # init rendering
-        if self.window_surface is None:
+        if self._window_surface is None:
             pygame.init()
 
         if self.render_mode == "human":
             pygame.display.init()
             pygame.display.set_caption("Pacman Maze")
-            self.window_surface = pygame.display.set_mode((self.window_size_w, self.window_size_h))
+            self._window_surface = pygame.display.set_mode((self.window_size_w, self.window_size_h))
         elif self.render_mode == "rgb_array":
-            self.window_surface = pygame.Surface((self.window_size_w, self.window_size_h))
+            self._window_surface = pygame.Surface((self.window_size_w, self.window_size_h))
 
-        assert(self.window_surface is not None)
+        assert(self._window_surface is not None)
 
-        if self.clock is None:
-            self.clock = pygame.time.Clock()
+        if self._clock is None:
+            self._clock = pygame.time.Clock()
 
         # load images
-        if self.background_image is None:
+        if self._background_image is None:
             file_name = os.path.join(IMGS_DIR, "back.webp")
-            self.background_image = pygame.transform.scale(
-                pygame.image.load(file_name), (self.cell_size, self.cell_size)
-            )
-        if self.goal_image is None:
-            file_name = os.path.join(IMGS_DIR, "flag.png")
-            self.goal_image = pygame.transform.scale(
+            self._background_image = pygame.transform.scale(
                 pygame.image.load(file_name), (self.cell_size, self.cell_size)
             )
         if self.agent_image is None:
@@ -339,12 +335,17 @@ class AvoidingArena(gym.Env):
             self.agent_image = pygame.transform.scale(
                 pygame.image.load(file_name), (self.cell_size, self.cell_size)
             )
-        if self.enemy_images is None:
+        if self._target_image is None:
+            file_name = os.path.join(IMGS_DIR, "flag.png")
+            self._target_image = pygame.transform.scale(
+                pygame.image.load(file_name), (self.cell_size, self.cell_size)
+            )
+        if self._enemy_images is None:
             file_names = [
                 os.path.join(IMGS_DIR, "enemy1.webp"),
                 os.path.join(IMGS_DIR, "enemy2.webp")
                 ]
-            self.enemy_images = [
+            self._enemy_images = [
                 pygame.transform.scale(
                     pygame.image.load(f_name), (self.cell_size, self.cell_size)
                 ) for f_name in file_names
@@ -356,23 +357,23 @@ class AvoidingArena(gym.Env):
                 img_pos = (x * self.cell_size, y * self.cell_size)
                 rect = (*img_pos, self.cell_size, self.cell_size)
 
-                self.window_surface.blit(self.background_image, img_pos)
-                if (pos == self.curr_pos).all():
-                    self.window_surface.blit(self.agent_image, img_pos)
-                elif (pos == self.goal_pos).all():
-                    self.window_surface.blit(self.goal_image, img_pos)
-                elif (pos == self.enemies).all(axis=1).any():
-                    self.window_surface.blit(self.enemy_images[(x+y)%2], img_pos)
+                self._window_surface.blit(self._background_image, img_pos)
+                if (pos == self._curr_pos).all():
+                    self._window_surface.blit(self._agent_image, img_pos)
+                elif (pos == self._target_pos).all():
+                    self._window_surface.blit(self._target_image, img_pos)
+                elif (pos == self._enemies_pos).all(axis=1).any():
+                    self._window_surface.blit(self._enemy_images[(x+y)%2], img_pos)
 
-                pygame.draw.rect(self.window_surface, (1, 50, 32), rect, 1)
+                pygame.draw.rect(self._window_surface, (1, 50, 32), rect, 1)
 
         if self.render_mode == "human":
             pygame.event.pump()
             pygame.display.update()
-            self.clock.tick(self.metadata["render_fps"])
+            self._clock.tick(self.metadata["render_fps"])
         elif self.render_mode == "rgb_array":
             return np.transpose(
-                np.array(pygame.surfarray.pixels3d(self.window_surface)), axes=(1, 0, 2)
+                np.array(pygame.surfarray.pixels3d(self._window_surface)), axes=(1, 0, 2)
             )
         return None
 
@@ -383,13 +384,13 @@ class AvoidingArena(gym.Env):
             s += "│ "
             for x in range(self.grid_x):
                 pos = np.array((x,y))
-                if (pos == self.curr_pos).all():
+                if (pos == self._curr_pos).all():
                     s += "C "
-                elif (pos == self.start_pos).all():
+                elif (pos == self._start_pos).all():
                     s += "S "
-                elif (pos == self.goal_pos).all():
+                elif (pos == self._target_pos).all():
                     s += "G "
-                elif (pos == self.enemies).all(axis=1).any():
+                elif (pos == self._enemies_pos).all(axis=1).any():
                     s += "E "
                 else:
                     s += "  "
