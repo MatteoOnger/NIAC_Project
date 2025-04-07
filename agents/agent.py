ec0c12735a2045816ab306bb3f1994d5c8e996d3
MatteoOnger <matteo.onger@studenti.unimi.it>
2025-04-07 15:25:48 +0200

    feat(agent): working on agent implementation

--- agents/agent.py
+++ agents/agent.py
@@ -23,13 +23,22 @@ class PolicyNet(nn.Module):
     """
     """
 
-    def __ini__(self):
-        #TODO:
-        pass
+    def __ini__(
+        self,
+        arena :AvoidingArena,
+        provenance :str='difftopkproofs',
+        edge_penality :float=0.1
+    ):
+        """
+        """
+        self.arena = arena
+        self.provenance = provenance
+        self.edge_penality = edge_penality
+        return
 
 
     def forward():
-        #TODO: tutto
+        #TODO: ...
         pass
 
 
@@ -48,14 +57,14 @@ class PolicyNet(nn.Module):
         ctx.add_facts("target", [(target_p[node], node) for node in self.nodes])
         ctx.add_facts(
             "node",
-            [(torch.clip(1 - enemy_p[node] - self.step_cost, min=0), node) for node in self.nodes],
+            [(torch.clip(1 - enemy_p[node] - self.edge_penality, min=0), node) for node in self.nodes],
         ) # <- self.step_cost> to penalise longer walks
 
         # rules defining edges according to valid moves
-        ctx.add_rule(f"edge(x, y, xp, y, {ACTION.RIGHT.value}) = node(x, y) and node(xp, y) and xp == x + 1")
-        ctx.add_rule(f"edge(x, y, x, yp, {ACTION.UP.value})    = node(x, y) and node(x, yp) and yp == y + 1")
-        ctx.add_rule(f"edge(x, y, xp, y, {ACTION.LEFT.value})  = node(x, y) and node(xp, y) and xp == x - 1")
-        ctx.add_rule(f"edge(x, y, x, yp, {ACTION.DOWN.value})  = node(x, y) and node(x, yp) and yp == y - 1")
+        ctx.add_rule(f"edge(x, y, xp, y, {self.arena.Actions.RIGHT.value}) = node(x, y) and node(xp, y) and xp == x + 1")
+        ctx.add_rule(f"edge(x, y, x, yp, {self.arena.Actions.UP.value})    = node(x, y) and node(x, yp) and yp == y + 1")
+        ctx.add_rule(f"edge(x, y, xp, y, {self.arena.Actions.LEFT.value})  = node(x, y) and node(xp, y) and xp == x - 1")
+        ctx.add_rule(f"edge(x, y, x, yp, {self.arena.Actions.DOWN.value})  = node(x, y) and node(x, yp) and yp == y - 1")
         
         # compute paths
         ctx.add_rule("path(x, y, x, y) = node(x, y)")
@@ -70,7 +79,7 @@ class PolicyNet(nn.Module):
 
         # get results
         res = list(ctx.relation("next_action"))
-        actions = [ACTION(res[i][1][0]) for i in range(len(res))]
+        actions = [self.arena.Actions(res[i][1][0]) for i in range(len(res))]
         probs = torch.tensor([res[i][0] for i in range(len(res))])
 
         tmp = {actions[i].name:round(float(probs[i]), 2) for i in range(len(res))}
@@ -87,7 +96,7 @@ class PolicyNet(nn.Module):
 
     def _neural_comp(self, rgb_array :np.ndarray) -> Dict[str:torch.Tensor]:
         cells = [extract_cell(rgb_array, node[0], node[1], self.cell_size) for node in self.nodes]
-        #TODO: tutto il resto...
+        #TODO: ...
         pass
 
 
@@ -109,8 +118,7 @@ class Agent():
         eps_decay :float=1000,
         lr :float=1e-4,
         tau :float=5e-3,
-        provenance :str='difftopkproofs',
-        step_cost :float=0.1
+        provenance :str='difftopkproofs'
     ):
         """
         """
@@ -128,10 +136,10 @@ class Agent():
         self.eps_decay = eps_decay
         self.lr = lr
         self.tau = tau
-        
-        self.provenance = provenance
-        self.step_cost = step_cost
 
+        self.provenance = provenance
+
+        self.training_steps_done = 0
         self.memory = Memory(self.memory_size)
         self.nodes = [(i,j) for i in range(self.grid_x) for j in range(self.grid_y)]
 
@@ -148,12 +156,112 @@ class Agent():
         return
 
 
-    def select_action(self, rgb_array :np.ndarray) -> AvoidingArena.Actions:
+    def select_action(self, rgb_array :torch.tensor) -> AvoidingArena.Actions:
         """
         """
-        action_scores, _ = self.policy_net(rgb_array)
+        action_scores = self.policy_net(rgb_array)
         action = torch.argmax(action_scores, dim=1)
         return action
+
+
+    def run(self) -> None:
+        """
+        """
+        for i in range(1, self.num_epochs + 1):
+            self.train_epoch(i)
+            self.test_epoch(i)
+        return None
+
+
+    def train_epoch(self, epoch :int=0) -> None:
+        """
+        """
+        if self.arena.render_mode != "rgb_array":
+            LOGGER.error("invalid <arena>'s render mode, must be 'rgb_array'")
+            raise ValueError("invalid <arena>'s render mode, must be 'rgb_array'")
+
+        iterator = tqdm(range(self.num_episodes))
+        counter_succ, counter_upd, sum_loss = 0, 0, 0.0
+
+        for i in iterator:
+            _ = self.arena.reset()
+            curr_state_image = torch.tensor(self.arena.render())
+
+            done = False
+            while not done:
+                # select next action
+                if random.rand() < self._eps_threshold():
+                    action = torch.tensor([self.arena.action_space.sample()])
+                else:
+                    action = self.select_action(curr_state_image)
+                
+                _, reward, terminated, truncated, _ = self.arena.step(action)
+                
+                done = terminated or truncated
+                next_state_image = None if done else self.arena.render()
+                
+                transition = Transition(curr_state_image, action, reward, done, next_state_image)
+                self.memory.push(transition)
+                
+                # update policy network's weights
+                loss = self._optimize()
+
+                # soft update of the target network's weights θ′ ← τ θ + (1 −τ )θ′
+                target_net_state_dict = self.target_net.state_dict()
+                policy_net_state_dict = self.policy_net.state_dict()
+                for key in policy_net_state_dict:
+                    target_net_state_dict[key] = policy_net_state_dict[key]*self.tau + target_net_state_dict[key]*(1-self.tau)
+                self.target_net.load_state_dict(target_net_state_dict)
+
+                # update counters
+                sum_loss += loss
+                counter_upd += 1
+                counter_succ += 1 if done and reward > 0 else 0
+
+                # update current state
+                curr_state_image = next_state_image
+
+            # print training progress
+            success_rate = (counter_succ / (i + 1)) * 100.0
+            avg_loss = sum_loss / counter_upd
+            iterator.set_description(f"[Train Epoch {epoch}] Avg Loss: {avg_loss}, Success: {counter_succ}/{i + 1} ({success_rate:.2f}%)")     
+        return None
+
+
+    def test_epoch(self, epoch :int=0) -> None:
+        """
+        """
+        iterator = tqdm(range(self.num_episodes))
+        counter_succ = 0, 0
+
+        for episode_i in iterator:
+            _ = self.arena.reset()
+            state_image = torch.tensor(self.arena.render())
+
+            done = False
+            while not done:
+                # select next action
+                action = self.select_action(state_image)
+                _, reward, terminated, truncated, _ = self.arena.step(action)
+            
+                done = terminated or truncated
+                state_image = torch.tensor(self.arena.render())
+
+                # update counter
+                counter_succ += 1 if done and reward > 0 else 0
+
+            # print test progress
+            success_rate = (counter_succ / (episode_i + 1)) * 100.0
+            iterator.set_description(f"[Test Epoch {epoch}] Success {counter_succ}/{episode_i + 1} ({success_rate:.2f}%)")
+        return None
+
+
+    def _eps_threshold(self) -> float:
+        """
+        """
+        eps = self.eps_end + (self.eps_start - self.eps_end) * math.exp(-1. * self.training_steps_done / self.eps_decay)
+        self.training_steps_done += 1
+        return eps
 
 
     def _optimize(self):
@@ -195,32 +303,3 @@ class Agent():
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()
         return loss.detach()
-
-
-    def train_epoch(self, arena :AvoidingArena):
-        if arena.render_mode != "rgb_array":
-            LOGGER.error("invalid <arena>'s render mode, must be 'rgb_array'")
-            raise ValueError("invalid <arena>'s render mode, must be 'rgb_array'")
-
-        global steps_done
-        counter_succ, counter_fail, counter_opt, sum_loss = 0, 0, 0, 0.0
-        iterator = tqdm(range(self.num_episodes))
-
-        for i in iterator:
-            _ = arena.reset()
-            curr_state_img = self.arena.render()
-
-            done = False
-            while done:
-                eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
-                    math.exp(-1. * steps_done / self.eps_decay
-                )
-                steps_done += 1
-
-                if torch.rand() < eps_threshold:
-                    action = torch.tensor([self.arena.action_space.sample()])
-                else:
-                    action = self.predict_action(curr_state_img)
-                
-                _, reward, terminated, truncated, _ = self.arena.step(action[0])
-                done = terminated or truncated
