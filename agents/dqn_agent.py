ac832986ead94a7d20cdf8cf05479babfb4fcf48
MatteoOnger <matteo.onger@studenti.unimi.it>
2025-05-06 09:54:18 +0200

    feat(Agent): add one dropout layer

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -79,6 +79,7 @@ class CellClassifier(torch.nn.Module):
         # dense layers
         x = self.dropout(x)
         x = self.relu(self.fc_1(x))  # -> (_, 256)
+        x = self.dropout(x)
         x = self.softmax(self.fc_2(x))  # -> (_, 4)
         return x
 
